132438db6191e09d972442ef96565aa9207dd9b5
Louis <louis.watrin@gmail.com>
2018-09-07 18:14:59 +0200

    Delete FormBundle and ListBundle to manage them with submodule

--- src/Builder/FormBundle/BuilderFormBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Builder\FormBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BuilderFormBundle extends Bundle
-{
-}
--- src/Builder/ListBundle/BuilderListBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Builder\ListBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BuilderListBundle extends Bundle
-{
-}
